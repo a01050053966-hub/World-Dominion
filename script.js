fb34002c9104529644a2c5b66569b517632c9e2f
a01050053966-hub <a01050053966@gmail.com>
2026-07-04 17:11:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+let money = 0;
+let power = 10;
+let country = "";
+
 function startGame() {
-  alert("게임 시작!");
+  country = document.getElementById("countrySelect").value;
+
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("game").style.display = "block";
+
+  document.getElementById("countryName").innerText = "국가: " + country;
+  updateUI();
+}
+
+function earnMoney() {
+  money += 10;
+  updateUI();
+}
+
+function upgrade() {
+  if (money >= 20) {
+    money -= 20;
+    power += 5;
+  } else {
+    alert("돈 부족");
+  }
+  updateUI();
+}
+
+function attack() {
+  let enemy = Math.floor(Math.random() * 20);
+
+  if (power > enemy) {
+    money += 30;
+    alert("승리!");
+  } else {
+    money -= 10;
+    alert("패배...");
+  }
+
+  updateUI();
+}
+
+function updateUI() {
+  document.getElementById("money").innerText = money;
+  document.getElementById("power").innerText = power;
 }
